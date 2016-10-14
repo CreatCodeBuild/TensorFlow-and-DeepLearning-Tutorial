d9fbdd528ebc571cb5c8da3c33a84864cc151a43
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-10-13 22:07:59 -0400

    加入了基本的 GET

--- PythonTips/generator.py
+++ PythonTips/generator.py
@@ -4,54 +4,80 @@
 那么这一期就给大家讲一讲Generator是什么？为什么要使用Generator？
 '''
 
-def example1():
-	def generator_function():
-		yield 1
-		yield 2
-		return 3
+# def example1():
+# 	def generator_function():
+# 		yield 1
+# 		yield 2
+# 		return 3
+#
+# 	generatorObject = generator_function()
+# 	print(next(generatorObject))
+# 	print(next(generatorObject))
+#
+# 	try:
+# 		print(next(generatorObject))
+# 	except:
+# 		pass
+#
+# def example2():
+# 	def generator_function(number):
+# 		yield 1
+# 		yield 2
+# 		return 3
+#
+# 	generatorObject = generator_function()
+# 	print(next(generatorObject))
+# 	print(next(generatorObject))
+#
+# 	try:
+# 		print(next(generatorObject))
+# 	except:
+# 		pass
+#
+#
+# example1()
+#
+#
+#
+#
+#
+# # Event Loop
+# class Event():
+#
+#
+#
+# loop = []
+# for event in loop:
+# 	handle(event)
+#
 
-	generatorObject = generator_function()
-	print(next(generatorObject))
-	print(next(generatorObject))
+import socket
+import time
 
-	try:
-		print(next(generatorObject))
-	except:
-		pass
+def get(path):
+	s = socket.socket()
+	s.connect(('localhost', 3000))
 
-def example2():
-	def generator_function(number):
-		yield 1
-		yield 2
-		return 3
+	request = 'GET %s HHTP/1.0\r\n\r\n' % path
+	s.send(request.encode())
 
-	generatorObject = generator_function()
-	print(next(generatorObject))
-	print(next(generatorObject))
+	chunks = []
+	while True:
+		chunk = s.recv(1000)
+		if chunk:
+			chunks.append(chunk)
+		else:
+			body = (b''.join(chunks)).decode()
+			print('--------------------------------------')
+			print(body)
+			print('--------------------------------------\n\n')
+			return
 
-	try:
-		print(next(generatorObject))
-	except:
-		pass
-
-
-example1()
-
-
-
-
-
-# Event Loop
-class Event():
-
-
-
-loop = []
-for event in loop:
-	handle(event)
-
-
-
+start = time.time()
+get('/super-slow')
+get('/super-slow')
+get('/super-slow')
+print('%.1f sec' % (time.time() - start))
 
 
 
