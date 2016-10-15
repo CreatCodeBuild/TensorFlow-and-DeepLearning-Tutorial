382daa3d1a63cfc03eab052ea9bc3a308acce760
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-10-15 04:47:23 -0400

    Coroutine 框架写好啦！

--- PythonTips/coroutine.py
+++ PythonTips/coroutine.py
@@ -1,32 +1,45 @@
-import socket
+###
+#2# 我们来写一个简单的 Coroutine 框架。
+#3# 虽然Python有Coroutine库。
+#3# 但是，编程嘛，最主要的是开心！
+###
+
+import socket			# on top of TCP
 import time
 from selectors import DefaultSelector, EVENT_WRITE, EVENT_READ
+# select: System Call -----> watch the readiness of a unix-file(socket) i/o
+# only socket is possible in Windows
+# non-blocking socket
 
 
 selector = DefaultSelector()
 
 
-class Future:
+class Future:				# ~=Promise, return the caller scope a promise
+							# about something in the future
 	def __init__(self):
 		self.callbacks = []
 
-	def resolve(self):
+	def resolve(self):		# on future event callback
 		for func in self.callbacks:
 			func()
 
 
-class Task:	# responsible for calling next() on generators
+class Task:					# responsible for calling next() on generators
+							# in charge of the async functions
 	def __init__(self, gen, eventLoop):
 		self.gen = gen
 		self.step()
 
-	def step(self):
+	def step(self):			# go to next step/next yield
 		try:
-			f =	next(self.gen)
-		except StopIteration:
+			f = next(self.gen)
+			f.callbacks.append(self.step)
+		except StopIteration as e:
+			# task is finished
 			eventLoop.n_task -= 1
-			return
-		f.callbacks.append(self.step)
+			print('--------------------------------------', 'Byte Received:', e, '\n\n')
+
 
 
 class EventLoop:
@@ -41,8 +54,9 @@ class EventLoop:
 		while self.n_task:
 			events = selector.select()
 			for event, mask in events:
-				fut = event.data
-				fut.resolve()
+				f = event.data
+				f.resolve()
+
 
 
 def async_get(path):
@@ -54,41 +68,39 @@ def async_get(path):
 		print(e)
 
 	request = 'GET %s HHTP/1.0\r\n\r\n' % path
+
 	f = Future()
 	selector.register(s.fileno(), EVENT_WRITE, data=f)
-	yield f	# halt function state until s is writable
+	yield f
 
+	# the socket is writable
 	selector.unregister(s.fileno())
-	# s is writable
 	s.send(request.encode())
-	chunks = []
+
+	totalReceived = []
 	while True:
 		f = Future()
 		selector.register(s.fileno(), EVENT_READ, data=f)
 		yield f
-		# by now, s is readable
+
+		# socket is readable
 		selector.unregister(s.fileno())
-		chunk = s.recv(1000) # 1000 bytes
-		if chunk:
-			chunks.append(chunk)
+		received = s.recv(1000)
+		if received:
+			totalReceived.append(received)
 		else:
-			body = (b''.join(chunks)).decode()
+			body = (b''.join(totalReceived)).decode()
 			print('--------------------------------------')
 			print(body)
-			print('--------------------------------------\n\n')
-			return
+			return len(body)
 
+if __name__ == '__main__':
+	start = time.time()
+	eventLoop = EventLoop()
 
-start = time.time()
-eventLoop = EventLoop()
+	for i in range(20):
+		eventLoop.add_task(async_get('/super-slow'))
 
-eventLoop.add_task(async_get('/slow'))
-eventLoop.add_task(async_get('/slow'))
-eventLoop.add_task(async_get('/slow'))
-eventLoop.add_task(async_get('/slow'))
-eventLoop.add_task(async_get('/slow'))
-eventLoop.add_task(async_get('/slow'))
+	eventLoop.start()
 
-eventLoop.start()
-
-print('%.1f sec' % (time.time() - start))
+	print('%.1f sec' % (time.time() - start))
