65d53ea33a26d9e909c765e957c1f50178978fd0
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-10-31 02:15:09 -0400

    Better API

--- 12-15/dp_refined_api.py
+++ 12-15/dp_refined_api.py
@@ -58,7 +58,6 @@ class Network():
 		self.pooling_stride = pooling_scale
 
 		# Graph Related
-		self.graph = tf.Graph()
 		self.tf_train_samples = None
 		self.tf_train_labels = None
 		self.tf_test_samples = None
@@ -70,11 +69,10 @@ class Network():
 		self.test_summaries = []
 
 		# 初始化
-		self.define_inputs()
-		self.session = tf.Session(graph=self.graph)
+		# self.define_inputs()
 
 		# TensorBoard Visualization
-		self.writer = tf.train.SummaryWriter('./board', self.graph)
+
 
 	def add_conv(self, *, patch_size, in_depth, out_depth, activation='relu', pooling=False, name):
 		'''
@@ -88,13 +86,12 @@ class Network():
 			'pooling': pooling,
 			'name': name
 		})
-		with self.graph.as_default():
-			with tf.name_scope(name):
-				weights = tf.Variable(
-					tf.truncated_normal([patch_size, patch_size, in_depth, out_depth], stddev=0.1))
-				biases = tf.Variable(tf.constant(0.1, shape=[out_depth]))
-				self.conv_weights.append(weights)
-				self.conv_biases.append(biases)
+		with tf.name_scope(name):
+			weights = tf.Variable(
+				tf.truncated_normal([patch_size, patch_size, in_depth, out_depth], stddev=0.1), name=name+'_weights')
+			biases = tf.Variable(tf.constant(0.1, shape=[out_depth]), name=name+'_biases')
+			self.conv_weights.append(weights)
+			self.conv_biases.append(biases)
 
 	def add_fc(self, *, in_num_nodes, out_num_nodes, activation='relu', name):
 		'''
@@ -106,93 +103,94 @@ class Network():
 			'activation': activation,
 			'name': name
 		})
-		with self.graph.as_default():
-			with tf.name_scope(name):
-				weights = tf.Variable(tf.truncated_normal([in_num_nodes, out_num_nodes], stddev=0.1))
-				biases = tf.Variable(tf.constant(0.1, shape=[out_num_nodes]))
-				self.fc_weights.append(weights)
-				self.fc_biases.append(biases)
-				self.train_summaries.append(tf.histogram_summary(str(len(self.fc_weights))+'_weights', weights))
-				self.train_summaries.append(tf.histogram_summary(str(len(self.fc_biases))+'_biases', biases))
+		with tf.name_scope(name):
+			weights = tf.Variable(tf.truncated_normal([in_num_nodes, out_num_nodes], stddev=0.1))
+			biases = tf.Variable(tf.constant(0.1, shape=[out_num_nodes]))
+			self.fc_weights.append(weights)
+			self.fc_biases.append(biases)
+			self.train_summaries.append(tf.histogram_summary(str(len(self.fc_weights))+'_weights', weights))
+			self.train_summaries.append(tf.histogram_summary(str(len(self.fc_biases))+'_biases', biases))
 
 	# should make the definition as an exposed API, instead of implemented in the function
 	def define_inputs(self):
-		with self.graph.as_default():
-			# 这里只是定义图谱中的各种变量
-			with tf.name_scope('inputs'):
-				self.tf_train_samples = tf.placeholder(
-					tf.float32, shape=(self.batch_size, image_size, image_size, num_channels), name='tf_train_samples'
-				)
-				self.tf_train_labels = tf.placeholder(
-					tf.float32, shape=(self.batch_size, num_labels), name='tf_train_labels'
-				)
-				self.tf_test_samples = tf.placeholder(
-					tf.float32, shape=(self.test_batch_size, image_size, image_size, num_channels), name='tf_test_samples'
-				)
+		# 这里只是定义图谱中的各种变量
+		with tf.name_scope('inputs'):
+			self.tf_train_samples = tf.placeholder(
+				tf.float32, shape=(self.batch_size, image_size, image_size, num_channels), name='tf_train_samples'
+			)
+			self.tf_train_labels = tf.placeholder(
+				tf.float32, shape=(self.batch_size, num_labels), name='tf_train_labels'
+			)
+			self.tf_test_samples = tf.placeholder(
+				tf.float32, shape=(self.test_batch_size, image_size, image_size, num_channels), name='tf_test_samples'
+			)
 
 	def define_model(self):
 		'''
 		定义我的的计算图谱
 		'''
-		with self.graph.as_default():
-			def model(data_flow, train=True):
-				'''
-				@data: original inputs
-				@return: logits
-				'''
-				# Define Convolutional Layers
-				for i, (weights, biases, config) in enumerate(zip(self.conv_weights, self.conv_biases, self.conv_config)):
-					with tf.name_scope(config['name'] + 'model'):
-						with tf.name_scope('convolution'):
-							# default 1,1,1,1 stride and SAME padding
-							conv = tf.nn.conv2d(data_flow, filter=weights, strides=[1, 1, 1, 1], padding='SAME')
-							addition = conv + biases
-						if config['activation'] == 'relu':
-							data_flow = tf.nn.relu(addition)
-						else:
-							raise Exception('Activation Func can only be Relu right now. You passed', config['activation'])
-						if config['pooling']:
-							data_flow = tf.nn.max_pool(
-								data_flow,
-								ksize=[1, self.pooling_scale, self.pooling_scale, 1],
-								strides=[1, self.pooling_stride, self.pooling_stride, 1],
-								padding='SAME')
-						# if not train:
-						# 	visualize_filter_map(hidden, how_many=self.conv1_depth, name='conv1_relu')
+		def model(data_flow, train=True):
+			'''
+			@data: original inputs
+			@return: logits
+			'''
+			# Define Convolutional Layers
+			for i, (weights, biases, config) in enumerate(zip(self.conv_weights, self.conv_biases, self.conv_config)):
+				with tf.name_scope(config['name'] + '_model'):
+					with tf.name_scope('convolution'):
+						# default 1,1,1,1 stride and SAME padding
+						data_flow = tf.nn.conv2d(data_flow, filter=weights, strides=[1, 1, 1, 1], padding='SAME')
+						data_flow = data_flow + biases
+						if not train:
+							self.visualize_filter_map(data_flow, how_many=config['out_depth'], display_size=32//(i//2+1), name=config['name']+'_conv')
+					if config['activation'] == 'relu':
+						data_flow = tf.nn.relu(data_flow)
+						if not train:
+							self.visualize_filter_map(data_flow, how_many=config['out_depth'], display_size=32//(i//2+1), name=config['name']+'_relu')
+					else:
+						raise Exception('Activation Func can only be Relu right now. You passed', config['activation'])
+					if config['pooling']:
+						data_flow = tf.nn.max_pool(
+							data_flow,
+							ksize=[1, self.pooling_scale, self.pooling_scale, 1],
+							strides=[1, self.pooling_stride, self.pooling_stride, 1],
+							padding='SAME')
+						if not train:
+							self.visualize_filter_map(data_flow, how_many=config['out_depth'], display_size=32//(i//2+1)//2, name=config['name']+'_pooling')
 
-				# Define Fully Connected Layers
-				for i, (weights, biases, config) in enumerate(zip(self.fc_weights, self.fc_biases, self.fc_config)):
-					if i == 0:
-						shape = data_flow.get_shape().as_list()
-						data_flow = tf.reshape(data_flow, [shape[0], shape[1] * shape[2] * shape[3]])
-					with tf.name_scope(config['name'] + 'model'):
-						data_flow = tf.matmul(data_flow, weights) + biases
-						if config['activation'] == 'relu':
-							data_flow = tf.nn.relu(data_flow)
-						elif config['activation'] is None:
-							pass
-						else:
-							raise Exception('Activation Func can only be Relu or None right now. You passed', config['activation'])
-				return data_flow
+			# Define Fully Connected Layers
+			for i, (weights, biases, config) in enumerate(zip(self.fc_weights, self.fc_biases, self.fc_config)):
+				if i == 0:
+					shape = data_flow.get_shape().as_list()
+					data_flow = tf.reshape(data_flow, [shape[0], shape[1] * shape[2] * shape[3]])
+				with tf.name_scope(config['name'] + 'model'):
+					data_flow = tf.matmul(data_flow, weights) + biases
+					if config['activation'] == 'relu':
+						data_flow = tf.nn.relu(data_flow)
+					elif config['activation'] is None:
+						pass
+					else:
+						raise Exception('Activation Func can only be Relu or None right now. You passed', config['activation'])
+			return data_flow
 
-			# Training computation.
-			logits = model(self.tf_train_samples)
-			with tf.name_scope('loss'):
-				self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, self.tf_train_labels))
-				self.train_summaries.append(tf.scalar_summary('Loss', self.loss))
+		# Training computation.
+		logits = model(self.tf_train_samples)
+		with tf.name_scope('loss'):
+			self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, self.tf_train_labels))
+			self.train_summaries.append(tf.scalar_summary('Loss', self.loss))
 
-			# Optimizer.
-			with tf.name_scope('optimizer'):
-				self.optimizer = tf.train.GradientDescentOptimizer(0.0001).minimize(self.loss)
+		# Optimizer.
+		with tf.name_scope('optimizer'):
+			self.optimizer = tf.train.GradientDescentOptimizer(0.0001).minimize(self.loss)
 
-			# Predictions for the training, validation, and test data.
-			with tf.name_scope('train'):
-				self.train_prediction = tf.nn.softmax(logits, name='train_prediction')
-			with tf.name_scope('test'):
-				self.test_prediction = tf.nn.softmax(model(self.tf_test_samples, train=False), name='test_prediction')
+		# Predictions for the training, validation, and test data.
+		with tf.name_scope('train'):
+			self.train_prediction = tf.nn.softmax(logits, name='train_prediction')
+		with tf.name_scope('test'):
+			self.test_prediction = tf.nn.softmax(model(self.tf_test_samples, train=False), name='test_prediction')
 
-			self.merged_train_summary = tf.merge_summary(self.train_summaries)
-			# self.merged_test_summary = tf.merge_summary(self.test_summaries)
+		self.merged_train_summary = tf.merge_summary(self.train_summaries)
+		self.merged_test_summary = tf.merge_summary(self.test_summaries)
 
 	def run(self):
 		'''
@@ -209,7 +207,9 @@ class Network():
 				print(column[i] / np.sum(column), )
 			print('\n', np.sum(confusionMatrix), a)
 
-		with self.session as session:
+		self.writer = tf.train.SummaryWriter('./board', tf.get_default_graph())
+
+		with tf.Session(graph=tf.get_default_graph()) as session:
 			tf.initialize_all_variables().run()
 
 			### 训练
@@ -233,14 +233,12 @@ class Network():
 			confusionMatrices = []
 			for i, samples, labels in get_chunk(test_samples, test_labels, chunkSize=self.test_batch_size):
 				print('samples shape', samples.shape)
-				# result, summary = session.run(
-				# 	[self.test_prediction, self.merged_test_summary],
-				# result = session.run(
-				# 	[self.test_prediction],
-				# 	feed_dict={self.tf_test_samples: samples}
-				# )
-				result = self.test_prediction.eval(feed_dict={self.tf_test_samples: samples})
-				# self.writer.add_summary(summary, i)
+				result, summary = session.run(
+					[self.test_prediction, self.merged_test_summary],
+					feed_dict={self.tf_test_samples: samples}
+				)
+				# result = self.test_prediction.eval(feed_dict={self.tf_test_samples: samples})
+				self.writer.add_summary(summary, i)
 				accuracy, cm = self.accuracy(result, labels, need_confusion_matrix=True)
 				accuracies.append(accuracy)
 				confusionMatrices.append(cm)
@@ -262,26 +260,33 @@ class Network():
 		accuracy = (100.0 * np.sum(_predictions == _labels) / predictions.shape[0])
 		return accuracy, cm
 
-	def visualize_filter_map(self, tensor, *, how_many, name):
+	def visualize_filter_map(self, tensor, *, how_many, display_size, name):
+		print(tensor.get_shape)
 		filter_map = tensor[-1]
+		print(filter_map.get_shape())
 		filter_map = tf.transpose(filter_map, perm=[2, 0, 1])
-		filter_map = tf.reshape(filter_map, (how_many, 32, 32, 1))
-		self.test_summaries.append(tf.image_summary(name, tensor=filter_map, max_images=1))
+		print(filter_map.get_shape())
+		filter_map = tf.reshape(filter_map, (how_many, display_size, display_size, 1))
+		print(how_many)
+		self.test_summaries.append(tf.image_summary(name, tensor=filter_map, max_images=how_many))
 
 
 if __name__ == '__main__':
 	net = Network(train_batch_size=64, test_batch_size=500, pooling_scale=2)
+	net.define_inputs()
 
 	#
-	net.add_conv(patch_size=3, in_depth=1, out_depth=1, activation='relu', pooling=False, name='conv1')
-	net.add_conv(patch_size=3, in_depth=1, out_depth=1, activation='relu', pooling=True, name='conv2')
-	net.add_conv(patch_size=3, in_depth=1, out_depth=1, activation='relu', pooling=False, name='conv3')
-	net.add_conv(patch_size=3, in_depth=1, out_depth=1, activation='relu', pooling=True, name='conv4')
+	net.add_conv(patch_size=3, in_depth=num_channels, out_depth=16, activation='relu', pooling=False, name='conv1')
+	net.add_conv(patch_size=3, in_depth=16, out_depth=16, activation='relu', pooling=True, name='conv2')
+	net.add_conv(patch_size=3, in_depth=16, out_depth=16, activation='relu', pooling=False, name='conv3')
+	net.add_conv(patch_size=3, in_depth=16, out_depth=16, activation='relu', pooling=True, name='conv4')
 
 	# 4 = 两次 pooling, 每一次缩小为 1/2
 	# 16 = conv4 out_depth
-	net.add_fc(in_num_nodes=(image_size // 4) * (image_size // 4) * 1, out_num_nodes=1, activation='relu', name='fc1')
-	net.add_fc(in_num_nodes=1, out_num_nodes=10, activation=None, name='fc2')
+	net.add_fc(in_num_nodes=(image_size // 4) * (image_size // 4) * 16, out_num_nodes=16, activation='relu', name='fc1')
+	net.add_fc(in_num_nodes=16, out_num_nodes=10, activation=None, name='fc2')
+
+
 
 	net.define_model()
 	net.run()
